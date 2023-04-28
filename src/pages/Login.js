64c22f0144732f33564e9e6fa52a7a8528bec3b3
Rahul Thakur <rahul.t@chapter247.com>
2023-04-28 12:02:15 +0530

    facebook login code testing

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -15,7 +15,6 @@ function Login(){
   const [emailErr,setEmailErr] = useState('')
   const [passwordErr,setPasswordErr] = useState('')
   const [success,setSuccess] = useState('')
-  const [googleUser,setGoogleUser] = useState({})
 
   const isLoggedIn = localStorage.getItem('loginData');
   const isGmailLoggedIn = localStorage.getItem('gmailLogin');
@@ -63,8 +62,6 @@ function Login(){
   const handleCallback=(response)=>{
     console.log('response',response.credential);
     var userObject = jwtDecode(response.credential)
-    console.log(userObject);
-    setGoogleUser(userObject)
     navigate('/dashboard');
     localStorage.setItem('gmailLogin',JSON.stringify(userObject));
   }
@@ -82,6 +79,79 @@ function Login(){
       theme:'outline',size:'large'
     })
   }, []);
+
+
+  /* window.fbAsyncInit = function() {
+    window.FB.init({
+      appId      : '247992354441620',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v2.7'
+    });
+      
+    window.FB.AppEvents.logPageView();     
+  }; */
+
+ /*  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk')); */
+
+
+   function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+    console.log('statusChangeCallback');
+    console.log(response);                   // The current login status of the person.
+    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+      testAPI();
+    } else {                                 // Not logged into your webpage or we are unable to tell.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this webpage.';
+    }
+  }
+
+
+  function checkLoginState() {               // Called when a person is finished with the Login Button.
+    window.FB.getLoginStatus(function(response) {   // See the onlogin handler
+      statusChangeCallback(response);
+    });
+  }
+
+
+  window.fbAsyncInit = function() {
+    window.FB.init({
+      appId      : '247992354441620',
+      cookie     : true,                     // Enable cookies to allow the server to access the session.
+      xfbml      : true,                     // Parse social plugins on this webpage.
+      version    : 'v5.0'           // Use this Graph API version for this call.
+    });
+
+
+    window.FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
+      statusChangeCallback(response);        // Returns the login status.
+    });
+  };
+
+
+  (function(d, s, id) {                      // Load the SDK asynchronously
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+
+  function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+    console.log('Welcome!  Fetching your information.... ');
+    window.FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
+    });
+  }
   return(
     <>
       {!isLoggedIn ? 
@@ -117,36 +187,16 @@ function Login(){
           </form>
 
           <div id="googleLogin"></div>
-          {/* {googleUser && <div>
-            <img src={googleUser.picture} alt="profile image" width={100} height={100}/>
-            <h2>{googleUser.given_name}{googleUser.family_name}</h2>
-            </div>
-          } */}
 
-          {/* <div id="g_id_onload"
-            data-client_id="YOUR_GOOGLE_CLIENT_ID"
-            data-login_uri="https://your.domain/your_login_endpoint"
-            data-auto_prompt="false">
-          </div> */}
-          {/* <div className="g-signin2" dataonsuccess={onSignIn}></div> */}
 
-          {/* <div id="g_id_onload"
-            data-client_id="1059940744811-dqu0n7q5gu8pdr3ug38eqk1gvus6i5c6.apps.googleusercontent.com"
-            data-context="signin"
-            data-ux_mode="popup"
-            data-login_uri="/dashboard"
-            data-callback={()=>onSignIn()}
-            data-itp_support="true">
-        </div>
+          {/* <div id="facebook-jssdk"></div>
 
-        <div className="g_id_signin"
-            data-type="standard"
-            data-shape="rectangular"
-            data-theme="outline"
-            data-text="signin_with"
-            data-size="large"
-            data-logo_alignment="left">
-        </div> */}
+          <div className="fb-login-button" data-width="100" data-size="" data-button-type="" data-layout="" data-auto-logout-link="true" data-use-continue-as="false"></div> */}
+
+<div id="fb-root"></div>
+<div className="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="true"></div>
+<div id="status"></div>
+
         </div>
       </div>
       :null}
